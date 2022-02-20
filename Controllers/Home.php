5b497cb357ca4cfff3831c193162b00b6e1b349b
Luis mi <alexanderlara1996@gmail.com>
2022-02-20 13:06:44 -0500

    Guardando cambios

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -10,19 +10,20 @@ class Home extends Controllers
     public function __construct()
     {
         parent::__construct();
-        $this->homeModel = new HomeModel();        
+        $this->homeModel = new HomeModel();
     }
 
     public function home($params)
-    {       
+    {
         $data["title-page"] = "Mi Empresa";
         $data["empresa"] =  $this->homeModel->loadEmpresa();
         $this->views->getView($this, "home", $data);
     }
 
     // Insertar
-    public function addUser()
+    public function addUser($params)
     {
+        dep($params);
         echo "perro";
     }
 }
